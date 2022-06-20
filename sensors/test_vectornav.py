476c431688ef17a07de8b18f43ba01f6ade348c2
Smashroom <jeanpiere.demir@gmail.com>
2022-06-21 00:58:46 +0100

    Introduce an example for vn100 usage

---
+++ sensors/test_vectornav.py
@@ -0,0 +1,31 @@
+from sensors.vectornav import Vectornav
+
+import time
+from datetime import datetime
+
+if __name__ == '__main__':
+    print('Program started')
+
+    t0 = datetime.now()
+
+    # Change that to correct serial port id
+    port = '/dev/cu.usbserial-AK05WMX3'
+    baud = 115200
+
+    thread_imu = Vectornav(1, port, baud, t0)
+    thread_imu.start()
+
+    while True:
+        try:
+            # Get and print data from IMU thread at every 0.5 seconds.
+            time.sleep(0.5)
+            imu_data = thread_imu.output_data()
+            print(imu_data)
+
+        except KeyboardInterrupt:
+            print('Exit command received')
+            break
+    
+    thread_imu.end_thread()
+
+    print('Program closed')
