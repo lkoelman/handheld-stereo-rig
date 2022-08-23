ece04640a4b89b644e8026a4cbcff3adae88f5ae
Smashroom <jeanpiere.demir@gmail.com>
2022-08-24 00:29:48 +0200

    Update the starter script

--- ros2/docker-all-sensors/ros_entrypoint.sh
+++ ros2/docker-all-sensors/ros_entrypoint.sh
@@ -3,6 +3,8 @@ set -e
 
 # setup ros2 environment
 source "/opt/ros/$ROS_DISTRO/setup.bash"
+# To get rid of failing DDS issue
+export RMW_IMPLEMENTATION=rmw_fastrtps_cpp
 
 if [[ -z "$@" ]]
 then
@@ -11,16 +13,16 @@ then
     CURR_TIME=$(date +%Y%m%d_%H%M%S)
     echo $CURR_TIME
 
-    LOG_DIR=/root/data_log/$CURR_TIME
+    LOG_DIR=/root/data_log/${CURR_TIME}_$RANDOM
     # Create folder with time/datae
     mkdir -p $LOG_DIR
     export ROS_LOG_DIR=$LOG_DIR
-    
+
     # Specify ROS LOG folder with the same time/date
     cd $LOG_DIR
     echo "Folder has been created. The new directory:"
     echo $(pwd)
-    
+
     echo "No launch parameters specified. Starting all sensors ..."
     source $HOME/dev_ws/install/setup.bash
     # Launch file must be fully qualitifed path or <package> <launch file>
@@ -28,4 +30,3 @@ then
 else
     exec "$@"
 fi
-    
